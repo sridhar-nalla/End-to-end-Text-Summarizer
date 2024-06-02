393bc3cb10c92bb566128994f3c1570341551869
Sourangshu Pal <paulbindass@gmail.com>
2024-06-02 11:39:12 +0530

    testcoom add

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -6,7 +6,7 @@ logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
-
+#test com added
 
 
 logging.basicConfig(
